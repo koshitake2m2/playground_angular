9eb36fbb5b4c75372f02b77f6c2f8c9a0edc31c9
koshitake2m2 <takeru.koshimizu@bizreach.co.jp>
2023-05-25 12:52:32 +0900

    fix lib

--- my-workspace/projects/my-app1/src/app/app.module.ts
+++ my-workspace/projects/my-app1/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyLib1Module } from 'my-lib1';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MyLib1Module],
   providers: [],
   bootstrap: [AppComponent],
 })
--- my-workspace/projects/my-lib1/src/lib/my-lib1.component.ts
+++ my-workspace/projects/my-lib1/src/lib/my-lib1.component.ts
@@ -1,20 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'lib-my-lib1',
-  template: `
-    <p>
-      my-lib1 works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>my-lib1 works!</p> `,
+  styles: [],
 })
 export class MyLib1Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
