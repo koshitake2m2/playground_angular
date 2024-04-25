6145bd5f7546a7c1c67fdc52b3b87e114771d004
koshitake2m2 <blueling2m2dvp@gmail.com>
2024-04-25 21:31:25 +0900

    add my-ssr

--- my-ssr/src/app/app.routes.ts
+++ my-ssr/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { HelloPageComponent } from './home/hello-page/hello-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'hello',
+    component: HelloPageComponent,
+  },
+];
